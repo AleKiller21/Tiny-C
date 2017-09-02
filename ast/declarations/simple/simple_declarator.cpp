a5c0dcce3f9a3caa690a1022099bb44445acaedc
AleKiller21 <aleferrera@hotmail.com>
2017-09-02 11:26:34 -0600

    Avoid redundant declaration of simple variables in global scope

--- ast/declarations/simple/simple_declarator.cpp
+++ ast/declarations/simple/simple_declarator.cpp
@@ -8,32 +8,35 @@ string simple_declarator::to_string()
 
 void simple_declarator::validate_semantic()
 {
+    string id = get_id();
+    symbol* sym = sym_table.exist_symbol_in_current_scope(id);
+
     if(type == VOID)
     {
-        show_error("variable or field '" + get_id() + "' declared void");
+        show_error("variable or field '" + id + "' declared void");
         return;
     }
     
-    if(!validate_existance()) return;
-    //if(!validate_initialization()) return;
-    sym_table.add_symbol(get_id(), new symbol {type, get_position(), true} );
+    if(!validate_existance(id, sym)) return;
+    if(!validate_initialization()) return;
+
+    if(sym == NULL) sym_table.add_symbol(id, new symbol {type, get_position(), init != NULL ? true : false } );
+    if(init != NULL) sym->is_initialized = true;
 }
 
-bool simple_declarator::validate_existance()
+bool simple_declarator::validate_existance(string id, symbol* sym)
 {
-    string id = get_id();
-    symbol* symbol = sym_table.exist_symbol_in_current_scope(id);
-    if(symbol == NULL) return true;
+    if(sym == NULL) return true;
 
-    if(symbol->is_initialized && init != NULL)
+    if(sym->is_initialized && init != NULL)
     {
-        show_error("Redefinition of '" + id + "'\nprevious definition was found at line " + std::to_string(symbol->lineno));
+        show_error("Redefinition of '" + id + "'\nprevious definition was found at line " + std::to_string(sym->lineno));
         return false;
     }
 
     if(sym_table.get_scope_level() > 0)
     {
-        show_error("Redeclaration of '" + id + "'\nprevious declaration was found at line " + std::to_string(symbol->lineno));
+        show_error("Redeclaration of '" + id + "'\nprevious declaration was found at line " + std::to_string(sym->lineno));
         return false;
     }
 
@@ -43,5 +46,7 @@ bool simple_declarator::validate_existance()
 
 bool simple_declarator::validate_initialization()
 {
-
+    if(init == NULL) return true;
+    //TODO: Mandar a llamar el get_type de init y compararlo con el tipo del declarador
+    return true;    
 }
