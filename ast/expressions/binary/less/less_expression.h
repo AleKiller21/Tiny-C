e1617eb29105f133adf3a62bdeeb458b4db08136
AleKiller21 <aleferrera@hotmail.com>
2017-09-12 10:22:57 -0600

    Remove unnecessary rules from less_expression

--- ast/expressions/binary/less/less_expression.h
+++ ast/expressions/binary/less/less_expression.h
@@ -6,10 +6,7 @@
 class less_expression : public relational_expression
 {
 public:
-    less_expression(expression* expr1, expression* expr2, int position) : relational_expression(expr1, expr2, position, "<")
-    {
-        //TODO: inicializar las reglas
-    }
+    less_expression(expression* expr1, expression* expr2, int position) : relational_expression(expr1, expr2, position, "<") {}
 
     string to_string();
     int get_kind();
