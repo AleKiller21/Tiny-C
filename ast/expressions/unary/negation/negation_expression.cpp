cff5f4b68cff59109c8aa4ae6b4d7b202b42f59d
AleKiller21 <aleferrera@hotmail.com>
2017-09-16 16:43:32 -0600

    Generate code for negation expression

--- ast/expressions/unary/negation/negation_expression.cpp
+++ ast/expressions/unary/negation/negation_expression.cpp
@@ -17,3 +17,18 @@ id_attributes negation_expression::get_type()
     if(expr_type.semantic_fail) return expr_type;
     return { INT, false, SIMPLE, false };
 }
+
+asm_code *negation_expression::generate_code(stack_manager *manager)
+{
+    is_code = expr->is_code;
+    asm_code *expr_code = expr->generate_code(manager);
+
+    if(!expr->is_code)
+    {
+        expr_code->constant = expr_code->constant >= 1 ? 0 : 1;
+        return expr_code;
+    }
+
+    expr_code->code += "\tslt " + expr_code->place + ", " + expr_code->place + ", 1\n";
+    return expr_code;
+}
