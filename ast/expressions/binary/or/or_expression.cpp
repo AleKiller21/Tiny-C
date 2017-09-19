e4b239c24c58d52cc19d488e17c1f742caa99fb7
AleKiller21 <aleferrera@hotmail.com>
2017-09-18 18:10:50 -0600

    Finish code generation for or and xor expressions

--- ast/expressions/binary/or/or_expression.cpp
+++ ast/expressions/binary/or/or_expression.cpp
@@ -9,3 +9,60 @@ int or_expression::get_kind()
 {
     return OR_EXPR;
 }
+
+asm_code *or_expression::generate_code(stack_manager *manager)
+{
+    asm_code *expr1_code;
+    asm_code *expr2_code;
+    asm_code *expr_code;
+    string treg;
+    bool right_operand_first = false;
+
+    generate_operands_code(manager, &expr1_code, &expr2_code, &right_operand_first);
+    string code = evaluate_function_expressions(manager, expr1_code, expr2_code, right_operand_first);
+
+    if(!code.empty())
+    {
+        treg = reg_manager.get_register(false);
+        code += "\tor " + treg + ", " + expr1_code->place + ", " + expr2_code->place + "\n";
+    }
+
+    else if(!expr1->is_code && !expr2->is_code)
+    {
+        expr_code = new asm_code { "", "", expr1_code->constant | expr2_code->constant };
+        delete expr1_code, expr2_code;
+        return expr_code;
+    }
+
+    else if(expr1->is_code && expr2->is_code)
+    {
+        if(right_operand_first) code = expr2_code->code + expr1_code->code;
+        else code = expr1_code->code + expr2_code->code;
+
+        reg_manager.free_register(expr1_code->place);
+        reg_manager.free_register(expr2_code->place);
+        treg = reg_manager.get_register(false);
+        code += "\tor " + treg + ", " + expr1_code->place + ", " + expr2_code->place + "\n";
+    }
+
+    else if(expr1->is_code)
+    {
+        code = expr1_code->code;
+        reg_manager.free_register(expr1_code->place);
+        treg = reg_manager.get_register(false);
+        code += "\tori " + treg + ", " + expr1_code->place + ", " + std::to_string(expr2_code->constant) + "\n";
+    }
+
+    else
+    {
+        code = expr2_code->code;
+        treg = reg_manager.get_register(false);
+        code += "\tli " + treg + ", " + std::to_string(expr1_code->constant) + "\n";
+        code += "\tor " + treg + ", " + treg + ", " + expr2_code->place + "\n";
+        reg_manager.free_register(expr2_code->place);
+    }
+
+    expr_code = new asm_code { code, treg, -1 };
+    delete expr1_code, expr2_code;
+    return expr_code;
+}
--- ast/expressions/binary/xor/xor_expression.cpp
+++ ast/expressions/binary/xor/xor_expression.cpp
@@ -9,3 +9,60 @@ int xor_expression::get_kind()
 {
     return XOR_EXPR;
 }
+
+asm_code *xor_expression::generate_code(stack_manager *manager)
+{
+    asm_code *expr1_code;
+    asm_code *expr2_code;
+    asm_code *expr_code;
+    string treg;
+    bool right_operand_first = false;
+
+    generate_operands_code(manager, &expr1_code, &expr2_code, &right_operand_first);
+    string code = evaluate_function_expressions(manager, expr1_code, expr2_code, right_operand_first);
+
+    if(!code.empty())
+    {
+        treg = reg_manager.get_register(false);
+        code += "\txor " + treg + ", " + expr1_code->place + ", " + expr2_code->place + "\n";
+    }
+
+    else if(!expr1->is_code && !expr2->is_code)
+    {
+        expr_code = new asm_code { "", "", expr1_code->constant ^ expr2_code->constant };
+        delete expr1_code, expr2_code;
+        return expr_code;
+    }
+
+    else if(expr1->is_code && expr2->is_code)
+    {
+        if(right_operand_first) code = expr2_code->code + expr1_code->code;
+        else code = expr1_code->code + expr2_code->code;
+
+        reg_manager.free_register(expr1_code->place);
+        reg_manager.free_register(expr2_code->place);
+        treg = reg_manager.get_register(false);
+        code += "\txor " + treg + ", " + expr1_code->place + ", " + expr2_code->place + "\n";
+    }
+
+    else if(expr1->is_code)
+    {
+        code = expr1_code->code;
+        reg_manager.free_register(expr1_code->place);
+        treg = reg_manager.get_register(false);
+        code += "\txori " + treg + ", " + expr1_code->place + ", " + std::to_string(expr2_code->constant) + "\n";
+    }
+
+    else
+    {
+        code = expr2_code->code;
+        treg = reg_manager.get_register(false);
+        code += "\tli " + treg + ", " + std::to_string(expr1_code->constant) + "\n";
+        code += "\txor " + treg + ", " + treg + ", " + expr2_code->place + "\n";
+        reg_manager.free_register(expr2_code->place);
+    }
+
+    expr_code = new asm_code { code, treg, -1 };
+    delete expr1_code, expr2_code;
+    return expr_code;
+}
